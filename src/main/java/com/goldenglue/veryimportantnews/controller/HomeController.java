ed5766380b4395f6dc351d25cac784f2fb85bc54
Goldenglue <ember.traveller@gmail.com>
2018-04-30 18:54:51 +0300

    .

--- src/main/java/com/goldenglue/veryimportantnews/controller/HomeController.java
+++ src/main/java/com/goldenglue/veryimportantnews/controller/HomeController.java
@@ -5,10 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    /*@RequestMapping("/")
-    public String home() {
-        return "index";
-    }*/
 
     @RequestMapping("greeting")
     public String greeting() {
